e421b94cc683a07fef3d19164cec205782b2f7ca
Daniel Kral <dakral@protonmail.com>
2024-04-20 17:30:48 +0200

    Implement map layer with berlin outline

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import './assets/app.css'
+import 'leaflet/dist/leaflet.css'
+import {MapContainer, Polyline} from "react-leaflet";
+import coords, {maxLat, maxLongs, minLat, minLongs} from "./globals/outline.ts";
+import {LatLng, LatLngBounds} from "leaflet";
 
-const App = () => (
-    <>
-        <div className="sidebar"></div>
-        <div className="map-container"></div>
-        <div className="logo"></div>
-        <div className="timeline"></div>
+const App = () => {
+    const outlineCoords = coords.map(p => new LatLng(p[1], p[0]));
+    const mapBounds = new LatLngBounds(new LatLng(minLat, minLongs), new LatLng(maxLat, maxLongs));
+    const centerCoords = new LatLng((minLat + maxLat) / 2, (minLongs + maxLongs) / 2);
+
+    return <>
+        <div className="sidebar">
+        </div>
+        <div className="map-container">
+            <MapContainer bounds={mapBounds} center={centerCoords} minZoom={11} zoom={11} zoomSnap={0.25}
+                          style={{width: '100%', backgroundColor: '#242424'}}>
+                <Polyline positions={outlineCoords} pathOptions={{color: '#dedede', fillColor: 'none'}}></Polyline>
+                
+            </MapContainer>
+        </div>
     </>
-);
+};
 
 export default App
