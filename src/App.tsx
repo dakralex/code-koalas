eeb81ab28f255a43f28d48d4ecad38c209ebe45c
Gojira321 <p.davenport@gmx.at>
2024-04-20 18:10:09 +0200

    sliderpartially implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './assets/app.css'
+import Slider from './slider.tsx'
 
 const App = () => (
     <>
         <div className="sidebar"></div>
         <div className="map-container"></div>
         <div className="logo"></div>
-        <div className="timeline"></div>
+        <div className="timeline">
+          <Slider min={0} max={48} step={1} />
+        </div>
     </>
 );
 
