4006f4b46dc4435b7881e1dd705eb96dbb4b23de
Daniel Kral <dakral@protonmail.com>
2024-04-21 02:04:33 +0200

    Implement plotting of the bike lane base layer

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
-import './assets/app.css'; // Separate import statement for CSS file
+import './assets/app.css';
 import 'leaflet/dist/leaflet.css';
+import BASE_LAYER from '../data/processed/baseLayer.json';
+
+import {LatLng, LatLngBounds} from "leaflet";
 import {MapContainer, Polyline} from "react-leaflet";
 import coords, {maxLat, maxLongs, minLat, minLongs} from "./globals/outline.ts";
-import {LatLng, LatLngBounds} from "leaflet";
-
-//const COORDS = [...Array(15000)].map((_, i) => <Polyline key={i} positions={[[minLat , minLongs], [maxLat, maxLongs]]}/>);
+import {useEffect, useState} from "react";
 
 const App = () => {
     const outlineCoords = coords.map(p => new LatLng(p[1], p[0]));
     const mapBounds = new LatLngBounds(new LatLng(minLat, minLongs), new LatLng(maxLat, maxLongs));
     const centerCoords = new LatLng((minLat + maxLat) / 2, (minLongs + maxLongs) / 2);
+
+    const [lanes, setLanes] = useState([]);
+
+    useEffect(() => {
+        // @ts-ignore
+        const arr = Array.from(BASE_LAYER).map(item => {
+            // @ts-ignore
+            const latLngPts = item?.coos.map(pt => new LatLng(pt[1], pt[0]));
+
+            return <Polyline positions={latLngPts} />;
+        });
+
+        // @ts-ignore
+        setLanes(arr)
+    }, []);
 
     return (
         <>
@@ -19,6 +35,7 @@ const App = () => {
                 <MapContainer bounds={mapBounds} center={centerCoords} zoom={11} zoomSnap={0.25}
                               style={{width: '100%', backgroundColor: '#242424'}}>
                     <Polyline positions={outlineCoords} pathOptions={{color: '#dedede', fillColor: 'none'}}></Polyline>
+                    {...lanes}
                 </MapContainer>
             </div>
         </>
